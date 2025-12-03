015a11fcdd506f2ea090624f439f59c3ccd3e872
robin_rajan <robinkarriottu@gmail.com>
2025-12-03 15:03:09 +0530

    React project files all removed and hardcoded api urls as well removed

--- src/components/modals/index.ts
+++
@@ -1,6 +0,0 @@
-export { UnsubscribeModal } from "./UnsubscribeModal";
-export { ExitIntentNewsletter } from "./ExitIntentNewsletter";
-export { CookieConsent } from "./CookieConsent";
-export { EmailContentViewer } from "./EmailContentViewer";
-export { PrivacyPolicyModal } from "./PrivacyPolicyModal";
-export { TermsOfServiceModal } from "./TermsOfServiceModal";
